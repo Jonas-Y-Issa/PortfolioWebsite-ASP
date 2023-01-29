cdf092e2496184b17909dfd14909339c1f5666c4
jonas_y_issa <jonas.y.issa@gmail.com>
2023-01-29 15:09:26 +1100

    Added Stack Effect

--- wwwroot/js/bubbles.js
+++ wwwroot/js/bubbles.js
@@ -11,9 +11,7 @@ class Bubble{
         this.friction = friction;
         this.bubble = bubble;
         this.ind = ind;
-        //this.positionCurve();
-        //this.positionDefault();
-        //this.positionQuadratic(ind);
+        this.play = true;
         this.positionQuadratic(ind*0.1);
     }
     positionCurve(){
@@ -45,12 +43,12 @@ class Bubble{
         this.bubble.style.left = y + "px";
     }
     moveQuadratic(i){
-        var x1 = container.width-(this.bubble.clientWidth);
+        var x1 = container.width/(container.width/container.height);
         var y1 = 0;
-        var x2 = container.width-(this.bubble.clientWidth);
-        var y2 = container.height-(this.bubble.clientHeight);
+        var x2 = container.width/(container.width/container.height);
+        var y2 = container.height-200;
         var x3 = 0;
-        var y3 = container.height-(this.bubble.clientHeight);
+        var y3 = container.height-200;
         var xa = this.getPt(x1, x2, i);
         var ya = this.getPt(y1, y2, i);
         var xb = this.getPt(x2, x3, i);
@@ -65,13 +63,27 @@ class Bubble{
         var diff = n2 - n1;
         return n1 + (diff * perc);
     }   
+    moveCSS(){
+        this.bubble.animate([
+            { transform: 'scale(1)', background: 'red', opacity: 1, offset: 0 },
+            { transform: 'scale(.5) rotate(270deg)', background: 'blue', opacity: .5, offset: .2 },
+            { transform: 'scale(1) rotate(0deg)', background: 'red', opacity: 1, offset: 1 },
+          ], {
+            duration: 2000, //milliseconds
+            easing: 'ease-in-out', //'linear', a bezier curve, etc.
+            delay: 10, //milliseconds
+            iterations: Infinity, //or a number
+            direction: 'alternate', //'normal', 'reverse', etc.
+            fill: 'forwards' //'backwards', 'both', 'none', 'auto'
+          });
+    }    
     move(){
-        if (this.bubble.offsetTop >= container.height - this.bubble.clientHeight){ 
-            this.speed = 0;
-        }
-        else{
-            this.speed+=0.008;
-            this.moveQuadratic(this.speed+(this.ind*0.1));
+        this.speed+=0.01;
+        this.moveQuadratic(this.speed);
+
+        if (this.bubble.offsetLeft + (this.bubble.offsetWidth/2) <= (this.ind) * (this.bubble.offsetWidth)){ 
+            this.play = false;
+            return;
         }
     }
 }
@@ -79,13 +91,15 @@ class Bubble{
     var i = -1;
     bubbles.forEach(bubble => {
         i += 1;
-        bubble_list.push(new Bubble(0,.3, bubble, i));
+        bubble_list.push(new Bubble(0,.09, bubble, i));
     });
 
     function animate(){
         requestAnimationFrame(animate);
         bubble_list.forEach(bubble => {
-            bubble.move();
+            if (bubble.play){
+                bubble.move();
+            }
         });
     }
     animate();
